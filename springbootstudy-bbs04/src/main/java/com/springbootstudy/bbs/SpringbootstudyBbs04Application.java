ea568eef9d1ad259244a3fb4a61d9bad203bfdf9
jj <eorms7391@gmail.com>
2025-06-09 13:50:17 +0900

    0609_게시판 만들기(04~06) 강의자료 폴더

---
+++ springbootstudy-bbs04/src/main/java/com/springbootstudy/bbs/SpringbootstudyBbs04Application.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudyBbs04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstudyBbs04Application.class, args);
+	}
+
+}
---
+++ springbootstudy-bbs06/src/main/java/com/springbootstudy/bbs/domain/Reply.java
@@ -0,0 +1,23 @@
+package com.springbootstudy.bbs.domain;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/* 하나의 댓글 정보를 저장하는 댓글 DTO(Data Transfer Object) 클래스
+ * 댓글 정보를 저장하고 있는 reply 테이블의 컬럼과 1:1 맵핑되는 클래스
+ **/
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reply {
+	private int no;
+	private int bbsNo;
+	private String replyContent;
+	private String replyWriter;
+	private Timestamp regDate;		
+}
